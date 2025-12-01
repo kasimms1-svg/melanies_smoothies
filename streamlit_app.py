1dceac5407b9c227e6f43332cafebc45dd06ea18
kasimms1-svg <kasimms1@gmail.com>
2025-12-01 16:54:18 -0500

    Update streamlit_app.py
    Add Python package called requests. This library enables building and sending REST API calls.

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,14 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    # st.write(my_insert_stmt)
-    # st.stop
-
-    time_to_insert = st.button('Submit Order')
+        time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
 
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+# New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
